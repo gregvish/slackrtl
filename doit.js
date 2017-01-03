fe75978a2f38aa6eadfe22e54d87dee7e77021ee
Greg Vish <greg@armis.com>
2017-01-03 12:00:19 +0200

    Adapted new box id

--- doit.js
+++ doit.js
@@ -3,7 +3,7 @@
 // @namespace    http://blah
 // @version      0.1
 // @description  Support RTL/Hebrew in slack.com
-// @author       
+// @author
 // @match        https://*.slack.com/messages/*
 // @grant        GPL
 // ==/UserScript==
@@ -14,16 +14,16 @@ function should_input_be_rtl(element)
 {
     if (typeof element.value != "string") {
         return false;
-    }    
-    return null != element.value.match(/^[\s\d"']*[א-ת]+/);
+    }
+    return null !== element.value.match(/^[\s\d"']*[א-ת]+/);
 }
 
 function should_message_be_rtl(element)
 {
     if (typeof element.innerText != "string") {
         return false;
-    }    
-    return null != element.innerText.match(/\n[\s\d"']*[א-ת]+/);
+    }
+    return null !== element.innerText.match(/\n[\s\d"']*[א-ת]+/);
 }
 
 function fix_all_emojis(element)
@@ -33,7 +33,7 @@ function fix_all_emojis(element)
     while (emoji_elemnt) {
         emoji_elemnt.style.setProperty("direction", "ltr");
         emoji_elemnt = iterator.iterateNext();
-    };   
+    }
 }
 
 var observer = new MutationObserver(function(mutations) {
@@ -41,13 +41,13 @@ var observer = new MutationObserver(function(mutations) {
         for (var i = 0; i < mutation.addedNodes.length; i++) {
             var added = mutation.addedNodes[i];
             var targets = [];
-            
+
             if (added.nodeName == 'TS-MESSAGE') {
                 targets = [added];
             } else if (typeof added.getElementsByTagName == "function") {
                 targets = added.getElementsByTagName("TS-MESSAGE");
             }
-            
+
             for (var j = 0; j < targets.length; j++) {
                 var element = targets[j];
                 if (should_message_be_rtl(element)) {
@@ -56,11 +56,11 @@ var observer = new MutationObserver(function(mutations) {
                 }
             }
         }
-    })
+    });
 });
 observer.observe(document.getElementById("msgs_div"), { childList: true, subtree: true });
 
-document.getElementById("message-input").addEventListener("keyup", function(event) {
+document.getElementById("msg_input").addEventListener("keyup", function(event) {
     var target = event.target;
     if (should_input_be_rtl(target)) {
         target.style.setProperty("direction", "rtl");
