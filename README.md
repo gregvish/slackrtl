49473b21544dd6d82519d1c94388cdf37232795e
Gregory Vishnepolsky <greg@zafran.io>
2023-05-20 10:46:26 +1200

    Revived this for current slack version

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# slackrtl
-Slack RTL support for Hebrew messages
+# Slackrtl (Hebrew)
+Slack RTL support for Hebrew messages (revived!)
 
-This is a shitty Tampermonkey script that fixes RTL in slack in a "smart" way.
-
-Only messages that start (sort of) with hebrew text will be RTL'd. The rest are untouched.
-
+This is a slow Tampermonkey script that fixes Hebrew RTL in slack in a "smart" way.
--- doit.js
+++ doit.js
@@ -1,13 +1,11 @@
 // ==UserScript==
-// @name         Slack RTL
-// @namespace    http://blah
-// @version      0.1
-// @description  Support RTL/Hebrew in slack.com
-// @author
-// @match        https://*.slack.com/messages/*
-// @grant        GPL
+// @name         Slack RTL (Hebrew)
+// @namespace    http://slackrtl
+// @version      0.2
+// @description  Support Hebrew on slack.com
+// @author       gregvish
+// @match        https://app.slack.com/client/*
 // ==/UserScript==
-/* jshint -W097 */
 'use strict';
 
 function should_input_be_rtl(element)
@@ -26,48 +24,55 @@ function should_message_be_rtl(element)
     return null !== element.innerText.match(/^[\s\d"']*[א-ת]+/);
 }
 
-function fix_all_emojis(element)
+function do_messages_modification()
 {
-    var iterator = document.evaluate('//*[@class="emoji emoji-sizer"]', element, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-    var emoji_elemnt = iterator.iterateNext();
-    while (emoji_elemnt) {
-        emoji_elemnt.style.setProperty("direction", "ltr");
-        emoji_elemnt = iterator.iterateNext();
-    }
-}
+    var i;
+    var target;
+    var elems = document.getElementsByClassName("p-rich_text_section");
 
-function do_mod()
-{
-    var targets = document.getElementsByClassName("c-message__body");
+    for (i=0; i < elems.length; i++) {
+        target = elems[i];
 
-    for (var j = 0; j < targets.length; j++) {
-        var element = targets[j];
-
-        if (should_message_be_rtl(element)) {
-            element.parentNode.style.setProperty("direction", "rtl");
-            fix_all_emojis(element);
+        if (should_message_be_rtl(target)) {
+            target.style.setProperty("direction", "rtl");
+            target.style.setProperty("text-align", "right");
         }
     }
 }
 
-var observer = new MutationObserver(function(mutations) {
-    do_mod();
-});
-
-window.setTimeout(function() {
-    observer.observe(document.getElementsByClassName("c-virtual_list__scroll_container")[0], { childList: true, subtree: true });
-    window.console.log("Observer installed");
-    do_mod();
-}, 3000);
-
-document.getElementById("msg_input").addEventListener("keyup", function(event) {
+function editor_onkeyup(event)
+{
     var target = event.target;
+
     if (should_input_be_rtl(target)) {
         target.style.setProperty("direction", "rtl");
         target.style.setProperty("text-align", "right");
-        fix_all_emojis(target);
+
     } else {
         target.style.setProperty("direction", "ltr");
         target.style.setProperty("text-align", "left");
     }
+}
+
+function setup_editor_modification()
+{
+    var i;
+    var elems = document.getElementsByClassName("ql-editor")
+
+    for (i=0; i < elems.length; i++) {
+        elems[i].onkeyup = editor_onkeyup;
+    }
+}
+
+var observer = new MutationObserver(function(mutations) {
+    do_messages_modification();
+    setup_editor_modification();
 });
+
+window.setTimeout(
+    function() {
+        observer.observe(document.body, {childList: true, subtree: true});
+    },
+    1000
+);
+
